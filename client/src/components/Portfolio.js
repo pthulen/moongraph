1f99db111a225fe2e62ccb94d8b57253a9b6443e
Patrick Thulen <pat@Patricks-MacBook-Pro.local>
2021-05-24 16:39:39 -0700

    total portfolio value displayed

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -9,10 +9,20 @@ class Portfolio extends Component {
             this.props.fetchPortfolio();
             //this.props.addPortfolioCoin("ethereum","4");
     }
+    getPortfolioValue() {
+        if(this.props.portfolio && this.props.portfolio.length > 0){
+            const portfolio= this.props.portfolio;
+            let values= [];
+            portfolio[0].presentData.coinData.forEach(coin => values.push(coin.currentValue))
+            let totalValue = values.reduce((acc, currentValue) => acc + currentValue);
+            return totalValue;
+        }
+    }
     render() {
         return (
             <div>
                 <h4>Logged In!</h4>
+                <h5>Portfolio Value: ${this.getPortfolioValue()}</h5>
                 <CoinAsset />
                 <CoinUpdate />
             </div>

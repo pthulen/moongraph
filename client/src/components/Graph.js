0462f05db6d4e9f15978eb0d3a662818716e77b0
Patrick Thulen <pat@Patricks-MacBook-Pro.local>
2021-06-14 15:31:46 -0700

    graph component changes

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -3,7 +3,66 @@ import { Line } from "react-chartjs-2";
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-function getPortfolioValue() {
+class Graph extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    labels: [],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [],
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)"
+    },
+    {
+      label: "Second dataset",
+      data: [0,0,0,0,0,0,0,0,0,33, 25, 35, 51, 54, 76],
+      fill: false,
+      borderColor: "#742774"
+    }
+  ],
+  responsive: true
+  }
+}
+componentDidMount(){
+  this.addDateLabels()
+  this.addPortfolioValues();
+}
+
+//fill in total portfolio values
+addPortfolioValues() {
+  let portfolioValues = []
+  if(this.props.portfolio && !this.props.portfolio[0].pastData){
+    portfolioValues = [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0];
+    portfolioValues.push(this.getPortfolioValue())
+  }
+  //create a function that returns the graph data and simplify the state data
+  //update the graph data in state
+  // this.setState(prevState => ({
+  //   datasets: [
+  //     ...prevState.datasets,
+  //     [prevState.datasets[0].data = portfolioValues]
+  //   ]
+  // }));
+  // console.log(this.state.datasets)
+}
+//generate last 14 dates
+addDateLabels() {
+  let labels = []
+  for(let i= 14; i > -1; i--){
+    let d = new Date();
+    d.setDate(d.getDate()-i)
+    labels.push(d.toLocaleDateString())
+
+  }
+  this.setState({
+    labels: labels
+  })
+}
+
+getPortfolioValue() {
   if(this.props.portfolio && this.props.portfolio.length > 0 && this.props.coins){
     const portfolio= this.props.portfolio[0].presentData.coinData;
     //map over portfolio to gather current coin ids and their amounts
@@ -31,45 +90,15 @@ function getPortfolioValue() {
     return totalValue.toFixed(2);
 } 
 }
-//let portfolioValue = getPortfolioValue();
-let data = {
-  labels: [],
-  datasets: [
-    {
-      label: "First dataset",
-      data: [0,0,0,0,0,0,0,0,0,33, 53, 85, 41, 2000, 3829.02],
-      fill: true,
-      backgroundColor: "rgba(75,192,192,0.2)",
-      borderColor: "rgba(75,192,192,1)"
-    },
-    {
-      label: "Second dataset",
-      data: [0,0,0,0,0,0,0,0,0,33, 25, 35, 51, 54, 76],
-      fill: false,
-      borderColor: "#742774"
-    }
-  ],
-  responsive: true
-};
-
-//generate last 14 dates
-function addDateLabels() {
-  for(let i= 14; i > -1; i--){
-    let d = new Date();
-    d.setDate(d.getDate()-i)
-    data.labels.push(d.toLocaleDateString())
-
-  }
-}
-addDateLabels();
 
 
-
-class Graph extends Component {
 
     render() {
         return (
-                <Line data={data} />
+          <div>
+            <Line data={this.state} />
+            <p>{this.test}</p>
+          </div>
         );
     }
 }
